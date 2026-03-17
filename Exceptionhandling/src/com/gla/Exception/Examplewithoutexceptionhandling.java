d75b7ef2a49fd53b4b066ba396a41c69ce42108f
bhoomigupta265-gif <bhoomigupta265@gmail.com>
2026-03-17 15:54:21 +0530

    added exception handling in workshop

---
+++ Exceptionhandling/src/com/gla/Exception/Examplewithoutexceptionhandling.java
@@ -0,0 +1,10 @@
+package com.gla.Exception;
+
+public class Examplewithoutexceptionhandling {
+    public static void main(String[]args){
+        int a=10;
+        int b=0;
+        int c=a/b;
+        System.out.println("result: "+c);
+    }
+}
